b2c660dc78889ec80d550b35c8e11ac74bb1c86b
JoanLazo <joan162581@gmail.com>
2019-06-22 18:52:28 -0500

    Updates

---
+++ precache-manifest.36e37555e19de489ee7a066c210d5984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027180d584455b0575e32f534958f582",
+    "url": "/certitrust/index.html"
+  },
+  {
+    "revision": "a554a858c94b50e6d00a",
+    "url": "/certitrust/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "278c9af4ee37b9e38901",
+    "url": "/certitrust/static/js/2.afa3210f.chunk.js"
+  },
+  {
+    "revision": "a554a858c94b50e6d00a",
+    "url": "/certitrust/static/js/main.a483c9dc.chunk.js"
+  },
+  {
+    "revision": "870e25ef9d73a2781ee0",
+    "url": "/certitrust/static/js/runtime~main.969c45cf.js"
+  }
+]);
