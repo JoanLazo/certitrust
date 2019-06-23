453daf57b458643ec659b1b3086f047e2efadb13
JoanLazo <joan162581@gmail.com>
2019-06-23 01:14:06 -0500

    cambiando imagen de codigo qr

--- src/components/CertificadoVirtual.js
+++ src/components/CertificadoVirtual.js
@@ -8,7 +8,7 @@ const CertificadoVirtual = () => {
     <div className="row">
         <div className="col-12">
    <div className="d-flex justify-content-center">
-   <img className="" src="https://user-images.githubusercontent.com/44485081/59970782-fce13880-9533-11e9-94ac-37205826455a.jpeg" alt="constancia" />
+   <img className="" src="https://user-images.githubusercontent.com/44485081/59972285-e1852600-9551-11e9-8600-9ec4afb0b749.jpeg" alt="constancia" />
    </div>
    
 
